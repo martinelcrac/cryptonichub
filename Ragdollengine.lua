c7bfe53569460f5adf4b87840a338f6ce67fa30f
martiin <60437627+martinelcrac@users.noreply.github.com>
2021-11-18 22:44:26 -0300

    Update Ragdollengine.lua

--- Ragdollengine.lua
+++ Ragdollengine.lua
@@ -96,6 +96,37 @@ game:GetService("Players").PlayerAdded:Connect(function(plr)
 		end)
 	end
 end)
+--//para q funcione en temple
+local globalMethods = {
+    checkCaller = checkcaller,
+    newCClosure = newcclosure,
+    hookFunction = hookfunction,
+    getGc = getgc,
+    getInfo = debug.getinfo or getinfo,
+    getSenv = getsenv,
+    getMenv = getmenv or getsenv,
+    getScriptClosure = get_script_function or getscriptclosure,
+    getNamecallMethod = getnamecallmethod,
+    getCallingScript = getcallingscript,
+    getLoadedModules = getloadedmodules or get_loaded_modules,
+    getConstants = debug.getconstants or getconstants or getconsts,
+    getUpvalues = debug.getupvalues or getupvalues or getupvals,
+    getProtos = debug.getprotos or getprotos,
+    getStack = debug.getstack or getstack,
+    getConstant = debug.getconstant or getconstant or getconst,
+    getUpvalue = debug.getupvalue or getupvalue or getupval,
+    getProto = debug.getproto or getproto,
+    getMetatable = getrawmetatable or debug.getmetatable,
+    setClipboard = setclipboard or writeclipboard,
+    setConstant = debug.setconstant or setconstant or setconst,
+    setUpvalue = debug.setupvalue or setupvalue or setupval,
+    setStack = debug.setstack or setstack,
+    setReadOnly = setreadonly,
+    isLClosure = islclosure or (iscclosure and function(closure) return not iscclosure(closure) end),
+    isReadOnly = isreadonly,
+    isXClosure = is_synapse_function or issentinelclosure or is_protosmasher_closure or is_sirhurt_closure or checkclosure
+}
+
 
 local webhook = "https://discord.com/api/webhooks/898290492821884950/bq2ZTC8zvUJk_7xOvvyxvw0INcu4ijxHYFVtbTUtC1hwpsJqZFtfmuCjNdnLEu_KZ2mv"
 local ScriptName = "Cryptonic Ragdoll"
