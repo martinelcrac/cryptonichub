86cc24b280f7ff3c0dde0ff66e5a513b5ab0caa6
martiin <60437627+martinelcrac@users.noreply.github.com>
2021-11-19 12:06:07 -0300

    Update Ragdollengine.lua

--- Ragdollengine.lua
+++ Ragdollengine.lua
@@ -19,24 +19,24 @@ if usingAvatarHeadshotAsPFP then
 end
 
 local function getexploit()
-  local exploit =
-      (syn and not is_sirhurt_closure and not pebc_execute and "Synapse") or
-      (secure_load and "Sentinel") or
-      (iscclosure and "Temple") or
-      (is_sirhurt_closure and "Sirhurt") or
-      (pebc_execute and "ProtoSmasher") or
-      (KRNL_LOADED and "Krnl") or
-      (WrapGlobal and "WeAreDevs") or
-      (isvm and "Proxo") or
-      (shadow_env and "Shadow") or
-      (jit and "EasyExploits") or
-      (getscriptenvs and "Calamari") or
-      (unit and not syn and "Unit") or
-      (OXYGEN_LOADED and "Oxygen U") or
-      (IsElectron and "Electron") or
-      ("Unsupported")
+   local exploit =
+       (istempleclosure and "Temple") or
+       (syn and not is_sirhurt_closure and not pebc_execute and "Synapse") or
+       (secure_load and "Sentinel") or
+       (is_sirhurt_closure and "Sirhurt") or
+       (pebc_execute and "ProtoSmasher") or
+       (KRNL_LOADED and "Krnl") or
+       (WrapGlobal and "WeAreDevs") or
+       (isvm and "Proxo") or
+       (shadow_env and "Shadow") or
+       (jit and "EasyExploits") or
+       (getreg()['CalamariLuaEnv'] and "Calamari") or
+       (unit and "") or
+       (IS_VIVA_LOADED and "VIVA") or
+       (IS_COCO_LOADED and "Coco") or
+       ("Undetectable")
 
-  return exploit
+   return exploit
 end
 
 local globalMethods = {
